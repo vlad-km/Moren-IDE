14f45ca67672719a634702f2bf642f9bd5da11c4
vlad-km <gm5461@gmail.com>
2017-03-07 18:06:10 +0300

    CSS for scroll, fix variables.lisp test-batch

--- moren/tests/variables.lisp
+++ moren/tests/variables.lisp
@@ -1,4 +1,4 @@
-(batch-begin "Variables")
+(batch-begin "JSCL Variables in an browser asynchronous environment")
 
 (defvar *x*)
 (setq *x* 0)
@@ -7,9 +7,12 @@
               (test (= *x* 0))
               (setq *x* 1)
               (test (= *x* 1))
+              (test (= *x* 0))
               2)))
   (expected-failure (= *x* 2)))
 
 (expected-failure (= *x* 1))
+
+(test (= *x* 0))
 
 (batch-end)
